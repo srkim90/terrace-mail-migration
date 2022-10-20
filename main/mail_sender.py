0af9a0379c09ea72622e514211b1ac80788a4ae4
srkim <srkim@daou.co.kr>
2022-10-20 17:27:57 +0900

    commit : 20221020 메일 보내기 기능 # 2

--- main/mail_sender.py
+++ main/mail_sender.py
@@ -1,3 +1,4 @@
+from common_import import *
 from main.cmd.command_line_parser import read_sender_options
 from main.cmd.mail_sender_option_models import SenderCommandOptions
 from service.mail_send_service import send_all
