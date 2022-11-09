ff5711ae7359f95a4b8433aa3b5237ad219db358
srkim <srkim@daou.co.kr>
2022-11-09 10:42:28 +0900

    commit : 20221109 수정 #1

--- utils/create_test_user.py
+++ utils/create_test_user.py
@@ -11,14 +11,14 @@ def make_noti_off_data() -> bytes:
 
 def make_data(user_name: str) -> bytes:
     result: str = '''
-    {"name":"%s","loginId":"%s","password":"port2093@","positionId":"","gradeId":"","status":"online","employeeNumber":"","locale":"ko","mailGroup":"default","mailAddQuota":"0","quotaWarningMode":"","quotaWarningRatio":"90","quotaViolationAction":"","quotaOverlookRatio":"10","webFolderAddQuota":"0","forwardingUse":"","forwardingMode":"none","mailSenderUse":"","sendAllowMode":"group","maxSendMailCountUse":"group","maxSendMailCount":"100","inboxExpireDays":"none","trashExpireDays":"none","spamExpireDays":"none","approvalPassword":"","approvalLevel":"10","hiddenForwardingMode":"off","mailExpireDate":"","userVirtualDomains":"","repUserEmail":"%s@srkim.kr","directTel":"","mobileNo":"","webmailUsed":true,"popUsed":true,"imapUsed":true,"smtpAuthUsed":true,"additions":{"enName":"","jpName":"","zhcnName":"","zhtwName":"","viName":""},"groupMembers":[],"deptMembers":[{"deptId":16893}],"attPhoto":null,"approvalAttPhoto":null,"alternateAddr":[],"alternateUserDomain":[],"forwardingAddr":[],"sendAllowAddr":[],"hiddenForwardingAddr":[],"mailSenders":[],"configs":[{"name":"useAbbroadIpCheck","value":"fasle","valueType":"boolean"},{"name":"otpService","value":"false","valueType":"boolean"}],"birthday":"","lunarCal":false}
-    ''' % (user_name, user_name, user_name)
+    {"name":"김성래","loginId":"%s","password":"port2093@","positionId":"","gradeId":"","status":"online","employeeNumber":"","locale":"ko","mailGroup":"default","mailAddQuota":"0","quotaWarningMode":"","quotaWarningRatio":"90","quotaViolationAction":"","quotaOverlookRatio":"10","webFolderAddQuota":"0","forwardingUse":"","forwardingMode":"none","mailSenderUse":"","sendAllowMode":"group","maxSendMailCountUse":"group","maxSendMailCount":"100","inboxExpireDays":"none","trashExpireDays":"none","spamExpireDays":"none","approvalPassword":"","approvalLevel":"10","hiddenForwardingMode":"off","mailExpireDate":"","userVirtualDomains":"","repUserEmail":"%s@srkim.kr","directTel":"","mobileNo":"","webmailUsed":true,"popUsed":true,"imapUsed":true,"smtpAuthUsed":true,"additions":{"enName":"","jpName":"","zhcnName":"","zhtwName":"","viName":""},"groupMembers":[],"deptMembers":[{"deptId":16901}],"attPhoto":null,"approvalAttPhoto":null,"alternateAddr":[],"alternateUserDomain":[],"forwardingAddr":[],"sendAllowAddr":[],"hiddenForwardingAddr":[],"mailSenders":[],"configs":[{"name":"useAbbroadIpCheck","value":"fasle","valueType":"boolean"},{"name":"otpService","value":"false","valueType":"boolean"}],"birthday":"","lunarCal":false}
+    ''' % (user_name, user_name)
     return json.dumps(json.loads(result.encode('utf-8'))).encode('utf-8')
 
 
-def notify_off():
+def notify_off(start_idx: int, n_count: int):
     domaion = "https://nstaging.daouoffice.com"
-    for idx in range(1000, 2002):
+    for idx in range(start_idx, start_idx+n_count):
         uid = "srkim%d" % (idx,)
         sess = requests.session()
         result = sess.post(domaion + "/api/login",
@@ -30,18 +30,19 @@ def notify_off():
         print("idx=%d, status: %d, result.text=" % (idx, result.status_code,))
     return
 
-def create_users():
+def create_users(start_idx: int, n_count: int):
     domaion = "https://nstaging.daouoffice.com:8443"
     data = {}
     sess = requests.session()
     result = sess.post(domaion + "/go/ad/api/login",
                        data=b'{"username":"srkim1000","password":"port2093@","returnUrl":""}',
                        headers={"Content-Type": "application/json"})
-    for idx in range(1000):
-        data = make_data("srkim%d" % (1003+idx,))
+    for idx in range(n_count):
+        data = make_data("srkim%d" % (start_idx+idx,))
         result = sess.post(domaion + "/go/ad/api/user", data=data, headers={"Content-Type": "application/json"})
         print("status: %d, result.text=\n%s" % (result.status_code, result.text))
 
 
 if __name__ == "__main__":
-    notify_off()
+    create_users(3002, 2000)
+    notify_off(3002, 2000)
