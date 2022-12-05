5d239179bb357682847187da8b174b9efe5e3a39
srkim <srkim@daou.co.kr>
2022-12-05 18:26:47 +0900

    new file:   push.sh

---
+++ push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git remote set-url origin https://github.com/srkim90/terrace-mail-migration.git
+git push
+git remote set-url origin http://git.daouoffice.co.kr/srkim/terrace-mail-migration.git
+git push
