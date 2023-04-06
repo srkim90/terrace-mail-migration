d97bec3998556f4c2dd32fa2f04f7a391680f3af
srkim90 <shshrka001@gmail.com>
2023-04-06 15:14:17 +0900

    commit: 20230406 -> 디스크 별 용량 임계치 설정 버그픽스 #2

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 CENTOS_VER=`cat /etc/redhat-release  | awk -F 'release' '{print $2}' | tr -d ' ' | cut -d '.' -f 1`
 if [ ${CENTOS_VER} == 6 ]; then
     PYTHON_VER=3.6.15
-elif [ ${CENTOS_VER} == 7 ]; then
+elif [ ${CENTOS_VER} == 7 ] || [ ${CENTOS_VER} == 8 ]; then
     PYTHON_VER=3.9.14
 else
     echo "Error. Invalid CensOS version!!"
